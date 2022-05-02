8942f28927fb5b58de7d8e3700095839e90c36ed
m-sagawa <m.shingaki@ks-japan.jp>
2022-05-02 10:08:04 +0900

    【練習問題】変数、定数

---
+++ src/jp/co/aforce/practice/PracticeTest1.java
@@ -0,0 +1,47 @@
+package jp.co.aforce.practice;
+
+public class PracticeTest1 {
+
+	public static void main(String args[]) {
+
+		 //練習問題2
+
+		System.out.println("Hello World!");
+
+
+		//練習問題3
+
+		int x = 11;
+			System.out.println("x="+ x);
+
+
+		//練習問題4
+
+		int y = 13;
+		int z = 17;
+
+			System.out.println("y=" + y);
+			System.out.println("z=" + z);
+
+
+		//練習問題5
+
+		int total = y+=z;
+
+			System.out.println(total);
+
+
+		//練習問題6
+
+
+		int number = 6;
+
+		int sum = total += number;
+
+			System.out.println(sum);
+
+
+
+	}
+
+}
